01e7b61b9be9198d9b82ce54a71630fa1d542071
OlgaKovalevskayaa <krishtaoivicho@mail.ru>
2022-08-21 21:20:49 +0300

    Выполнила домашнее задание №4

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const messagesContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+    const messageText = sendInput.value;
+
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('items');
+    newMessage.textContent = messageText;
+
+    if (event.key == 'Enter') {
+        messagesContainer.append(newMessage);
+    }
+
+    sendInput.value = '';
+
+    const collection = document.querySelectorAll('.items');
+    for (let elem of collection) {
+        elem.addEventListener('click', clickOnMe);
+
+        function clickOnMe() {
+            elem.classList.toggle('done');
+        };
+    };
+})
